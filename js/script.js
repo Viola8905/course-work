d62a9d54332f60ee88592eccb482b557eab278c7
Violetta <viola89055@gmail.com>
2021-11-25 23:09:39 +0200

    scroll to block

--- js/script.js
+++ js/script.js
@@ -8,6 +8,21 @@ window.addEventListener("scroll",function(){
 
 
 
+$('.header__menu a').on('click', function() {
+
+  let href = $(this).attr('href');
+
+  $('html, body').animate({
+      scrollTop: $(href).offset().top
+  }, {
+      duration: 400,   // по умолчанию «400» 
+      easing: "linear" // по умолчанию «swing» 
+  });
+
+  return false;
+});
+
+
 
 
 //for menu burger
